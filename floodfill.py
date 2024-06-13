40dac19980b05814fdd92442e709c88d0b047b88
norris03 <65301566+norris03@users.noreply.github.com>
2024-06-13 22:49:10 +0200

    Update floodfill.py

--- floodfill.py
+++ floodfill.py
@@ -1,9 +1,10 @@
 import numpy as np
+from constants import *
 def create_grid(game_state):
     if game_state == {}:
         return 
     grid = np.rot90(np.zeros((game_state["board"]["height"],game_state["board"]["width"])))
-    for c in game_state["you"]["body"][1:]:
+    for c in game_state["you"]["body"][1:-1]:
         grid[c["x"]][c["y"]] = 1
     for snake in game_state["board"]["snakes"]:
         for c in snake["body"]:
@@ -11,6 +12,7 @@ def create_grid(game_state):
     return grid
 
 def fill(game_state, x, y):
+    score = 0
     max_x=game_state["board"]["width"]-1
     max_y=game_state["board"]["height"]-1
     grid = create_grid(game_state)  
@@ -22,11 +24,14 @@ def fill(game_state, x, y):
         if 0 <= x <= max_x and 0 <= y <= max_y:
             if grid[x][y] == 0:
                 grid[x][y] = 4
+                score += 1
                 l.insert(0, (x, y + 1))
                 l.insert(0, (x, y - 1))
                 l.insert(0, (x + 1, y))
                 l.insert(0, (x - 1, y))
-    return (grid == 4).sum()-1
+        if score > max_flood_fill_score:
+            break
+    return score
 
 def flood_fill(game_state):
     return fill(game_state, game_state["you"]["body"][0]["x"], game_state["you"]["body"][0]["y"])
