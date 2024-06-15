ce8fc9ce95e81a8dfee45b62440260b5bfd0dc19
norris03 <65301566+norris03@users.noreply.github.com>
2024-06-15 15:01:02 +0200

    Update evaluate_game_state.py

--- evaluate_game_state.py
+++ evaluate_game_state.py
@@ -10,6 +10,13 @@ def evaluate_game_state(game_state):
         game_state_score = death_score
         x = game_state["you"]["body"][0]["x"]
         y = game_state["you"]["body"][0]["y"]
+        tx = game_state["you"]["body"][-1]["x"]
+        ty = game_state["you"]["body"][-1]["y"]
+        tx2 = game_state["you"]["body"][-2]["x"]
+        ty2 = game_state["you"]["body"][-2]["y"]
+        head = (x,y)
+        tail = (tx,ty)
+        tail2 = (tx2, ty2)
         length = game_state["you"]["length"]
         grid = create_grid(game_state)
         width = game_state["board"]["width"]
@@ -20,7 +27,6 @@ def evaluate_game_state(game_state):
         max_distance_to_food = board_length - 1
         min_distance_to_food = max_distance_to_food
         distance_to_food = max_distance_to_food
-        head = (x, y)
         for apple in game_state["board"]["food"]:
             goal = (apple["x"], apple["y"])
             if heuristic(head, goal) > food_in_reach_distance:
@@ -41,23 +47,28 @@ def evaluate_game_state(game_state):
 
         health_score = game_state["you"]["health"]/100
         health_bonus = 0
-        if health_score >= 0.99:
+        if health_score == 1:
             health_bonus = 1
 
         length_score = length/board_area
 
         number_of_enemies = len(game_state["board"]["snakes"])
 
-        if flood_fill_score > length_score:
-            game_state_score = (
-                w_distance_to_food*distance_to_food_score
-                +w_health_bonus*health_bonus                
-                +w_distance_from_center*distance_from_center_score             
-                +w_edge_penalty*edge_penalty+w_flood_fill*flood_fill_score                
-                +w_length*length_score+w_number_of_enemies*number_of_enemies
-            )
-        else:
-            game_state_score = flood_fill_score
+        tail_bonus = 0
+        if a_star(grid=grid, start = head, goal = tail) < float('inf') or a_star(grid=grid, start = head, goal = tail2) < float('inf'):
+            tail_bonus = 1
+
         
+        game_state_score = (
+                w_distance_to_food*distance_to_food_score                
+                +w_health_bonus*health_bonus                
+                +w_distance_from_center*distance_from_center_score         
+                +w_tail_bonus*tail_bonus
+                +w_edge_penalty*edge_penalty
+                +w_flood_fill*flood_fill_score  
+                +w_tail_bonus*tail_bonus              
+                +w_length*length_score
+                +w_number_of_enemies*number_of_enemies
+                +w_edge_penalty*edge_penalty
+            )
         return game_state_score
-    
