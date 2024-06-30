9db4486c546a65e0433935dd04c5b274f7585067
norris03 <65301566+norris03@users.noreply.github.com>
2024-06-30 17:51:43 +0200

    Update minimax.py

--- minimax.py
+++ minimax.py
@@ -2,6 +2,19 @@ from constants import *
 from evaluate_game_state import evaluate_game_state
 
 def minimax(node, depth, alpha, beta, our_turn):
+    """
+    Führt den Minimax-Algorithmus mit Alpha-Beta-Pruning durch, um den besten Zug zu berechnen
+
+    Args:
+        node (Node): Der gegebene Knoten im Spielbaum
+        depth (float): Die Tiefe, bis zu dem der Baum durchsucht werden soll
+        alpha (float): Die bisher beste Punktzahl
+        beta (float): Die bisher schlechteste Punktzahl
+        our_turn (bool): True, falls unsere Schlange am Zug ist, False sonst
+
+    Returns:
+        float: Die beste Punktzahl, die von dem gegebenen Knoten (Spielzustand) erreichbar ist
+    """
     if depth == 0 or node.game_state == {}:
         eval = evaluate_game_state(node.game_state)
         node.score_game_state(eval)
