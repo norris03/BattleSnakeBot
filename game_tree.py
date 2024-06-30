d03ff7d073987ac26cae34ce61d90748dff341e8
norris03 <65301566+norris03@users.noreply.github.com>
2024-06-30 17:50:59 +0200

    Update game_tree.py

--- game_tree.py
+++ game_tree.py
@@ -4,6 +4,17 @@ import itertools
 import copy
 
 def is_bad_move(game_state, move, snake_body):
+    """
+    Bestimmt, ob ein Zug für die Schlange, die mit dem gegebenen Schlangen-Körper korrespondiert, schlecht ist
+
+    Args:
+        game_state (dict): Der gegebene Spielzustand
+        move (str): Der zu überprüfende Zug
+        snake_body (list of dict): Die Liste der Körpersegmente der Schlange
+
+    Returns:
+        bool: True, wenn der Zug schlecht ist, False sonst
+    """
     max_x = game_state["board"]["width"] - 1
     max_y = game_state["board"]["height"] - 1
     x = snake_body[0]["x"]
@@ -38,24 +49,60 @@ def is_bad_move(game_state, move, snake_body):
     return False
 
 class Node:
+    """
+    Ein Knoten im Spielbaum, der einen bestimmten Spielzustand, den Zug, der zu diesem Spielzustand geführt hat und die Bewertung (Punktzahl) des Spielzustand repräsentiert
+
+    Attribute:
+        move (str): Der Zug, der zu diesem Spielzustand geführt hat
+        game_state (dict): Der aktualisierte Spielzustand
+        score (float): Die Bewertung (Punktzahl) des Spielzustands
+        children (list of Node): Kindknoten (folgende Spielzustände)
+    """
     move = ""
     game_state = {}
     score = death_score
     children = []
 
     def __init__(self, game_state, move, is_our_turn):
+        """
+        Initialisierung des Knoten
+
+        Args:
+            game_state (dict): Der gegebene Spielzustand
+            move (str): Der Zug (die Züge), der ausgeführt (die aussgeführt) werden soll(en)
+            is_our_turn (bool): True, wenn unsere Schlange am Zug ist, False sonst
+        """
         self.move = move
         self.game_state = update_game_state(game_state, move, is_our_turn)
         self.score = death_score
         self.children = []
 
     def add_child(self,node):
+        """
+        Fügt dem aktuellen Knoten einen Kindknoten hinzu
+
+        Args:
+            node (Node): Der hinzuzufügende Kindknoten
+        """
         self.children.append(node)
 
     def score_game_state(self, calculated_score):
+        """
+        Weist dem Knoten die gegebene Punktkzahl zu
+
+        Args:
+            calculated_score (float): Die Punktzahl die dem Knoten zugewiesen werden soll
+        """
         self.score = calculated_score
 
     def create_tree(self, depth, is_our_turn):
+        """
+        Erstellt einen Spielbaum
+
+        Args:
+            depth (float): Die Tiefe des zu erstellenden Baums
+            is_our_turn (bool): True, wenn unsere Schlange am Zug ist, False sonst
+        """
         if depth == 0 or self.game_state == {}:
             return
         legal_moves = ["up","right","down","left"]
