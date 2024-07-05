8271f4ea2b0f3cfc2b91e2885e2190009d126a49
norris03 <65301566+norris03@users.noreply.github.com>
2024-07-05 17:06:47 +0200

    Update floodfill.py

--- floodfill.py
+++ floodfill.py
@@ -1,3 +1,11 @@
+"""
+Baidinger, Artur 220201391
+Haufe, Hans-Hauke 222200976
+Hellerich, Hannes 222201181
+Löbnau, Norris 222200294
+Rieck, Damijan Ali 218200608
+Strompen, Zoe Katharina 221200152
+"""
 import numpy as np
 from collections import deque
 from constants import *
